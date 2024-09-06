8a22a66f15a8996767b28d1323ebd98405e5b3e5
Brazatlant <87148948+Brazatlant@users.noreply.github.com>
2024-09-06 23:52:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.addEventListener('mouseenter', function() {
+        this.querySelector('.dropdown-content').style.display = 'block';
+    });
+    dropdown.addEventListener('mouseleave', function() {
+        this.querySelector('.dropdown-content').style.display = 'none';
+    });
+});
